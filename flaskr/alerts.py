5db4e8aa17d7f865b52972da4e7fac98b114f5fe
Patrick Duvall <duvallp208@gmail.com>
2021-01-04 11:52:05 -0700

    Add alert edit form

--- flaskr/alerts.py
+++ flaskr/alerts.py
@@ -56,3 +56,53 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('alert/index.html', alerts=alerts)
+
+
+def get_alert(id):
+    alert = get_db().execute(
+        'SELECT a.id, title, message, email, schedule'
+        ' FROM alert a'
+        ' WHERE a.id = ?',
+        (id,)
+    ).fetchone()
+
+    if alert is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    return alert
+
+
+@bp.route('/<int:id>/edit', methods=['GET'])
+@login_required
+def edit(id):
+    alert = get_alert(id)
+    return render_template('alert/edit.html', alert=alert)
+
+
+@bp.route('/<int:id>/update', methods=['POST'])
+@login_required
+def update(id):
+
+    title = request.form['title']
+    message = request.form['message']
+    email = request.form['email']
+    schedule = request.form['schedule']
+    error = None
+
+    if not title:
+        error = 'Title is required.'
+
+    if not email:
+        error = 'Email is required.'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'UPDATE alert SET title = ?, message= ?, email= ?, schedule= ?'
+            ' WHERE id = ?',
+            (title, message, email, schedule, id)
+        )
+        db.commit()
+        return redirect(url_for('alerts.index'))
