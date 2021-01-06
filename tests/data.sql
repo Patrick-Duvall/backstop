db3d0b1a6612484b7f6d8f02cb1239c627417e47
Patrick Duvall <duvallp208@gmail.com>
2021-01-06 14:16:05 -0700

    add nav links, add nav to alert.edit alert.index

--- tests/data.sql
+++ tests/data.sql
@@ -6,3 +6,7 @@ VALUES
 INSERT INTO post (title, body, author_id, created)
 VALUES
   ('test title', 'test' || x'0a' || 'body', 1, '2018-01-01 00:00:00');
+
+INSERT INTO alert (title, email, message, schedule, author_id, created)
+VALUES
+  ('test title', 'test@test.com', 'test message', '2020-01-01 00:00:00', 1, '2018-01-01 00:00:00');
