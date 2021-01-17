65b09d4c9721e8079f6167cca766e8b4d6635eb5
Patrick Duvall <duvallp208@gmail.com>
2021-01-17 10:43:03 -0700

    rename application in init.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uwsgi --http :8000 --wsgi-file flaskr/__init__.py --master --processes 4 --threads 2
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,7 +8,7 @@ from flaskr.forms import AlertForm
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    application = app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
