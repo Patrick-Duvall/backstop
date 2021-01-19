a80e5db1888583f9505efc60b8d8c98f241b79da
Patrick Duvall <duvallp208@gmail.com>
2021-01-19 15:48:25 -0700

    add initial google login and callback

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,14 +1,33 @@
 import functools
+import json
+import os
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
 from flaskr.db import get_db
+
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_OAUTH_TEST_CLIENT", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_OAUTH_TEST_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+# TODO ADD ERROR HANDLING
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -38,6 +57,68 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+
+@bp.route('/google_login', methods=['GET'])
+def google_login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@bp.route("/google_login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    return redirect(url_for('alerts.index'))
 
 
 @bp.route('/login', methods=('GET', 'POST'))
