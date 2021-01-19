ace1b1117c96cacf03aad3121f582f46b44d3e4f
Patrick Duvall <duvallp208@gmail.com>
2021-01-19 09:20:15 -0700

    remove forms references

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -4,7 +4,6 @@ from flaskr.db import get_db
 
 def test_index(client, auth):
     import pdb;
-    pdb.set_trace()
     response = client.get('/')
     assert b"Log In" in response.data
     assert b"Register" in response.data
