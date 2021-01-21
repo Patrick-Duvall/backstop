b1bfc8691d64df50a7f80323fd010111a360f98f
Patrick Duvall <duvallp208@gmail.com>
2021-01-21 11:52:54 -0700

    Complete google oauth

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -43,36 +43,6 @@ def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 # TODO ADD ERROR HANDLING
 
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-                'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
-
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
-
-        flash(error)
-
-    return render_template('auth/register.html')
-
-
 @bp.route('/google_login', methods=['GET'])
 def google_login():
     # Find out what URL to hit for Google login
@@ -141,33 +111,6 @@ def callback():
     login_user(user)
 
     return redirect(url_for('alerts.index'))
-
-
-@bp.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('alerts.index'))
-
-        flash(error)
-
-    return render_template('auth/login.html')
-
 
 @bp.before_app_request
 def load_logged_in_user():
