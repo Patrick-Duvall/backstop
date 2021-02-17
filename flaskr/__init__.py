4202272062a6cf5f05edd072912bd91c6d684acd
Patrick Duvall <duvallp208@gmail.com>
2021-02-17 12:18:46 -0700

    Mailer works in one file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_mail import Mail, Message
 from apscheduler.schedulers.background import BackgroundScheduler
 import os
-
 from flask import Flask, render_template
 
 
@@ -38,28 +37,23 @@ def create_app(test_config=None):
     from . import alerts
     application.register_blueprint(alerts.bp, url_prefix="/alerts")
 
-    # from jobs import send_alert_email
-    # send_alert_email.send_overdue_emails()
-
     logging.basicConfig()
     logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
     mail = Mail(application)
-
     application.config['MAIL_SERVER'] = 'smtp.gmail.com'
     application.config['MAIL_PORT'] = 465
-    application.config['MAIL_USERNAME'] = 'backStopApp@gmail.com'
-    application.config['MAIL_PASSWORD'] = 'afh8afh8'
+    # import pdb; pdb.set_trace()
+    application.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    application.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     application.config['MAIL_USE_TLS'] = False
     application.config['MAIL_USE_SSL'] = True
     mail = Mail(application)
-
 
     def send_overdue_emails():
         with application.app_context():
             now = datetime.now()
             database = db.get_db()
-
             alerts = database.execute(
                 'SELECT id, title, schedule, email, message, sent'
                 ' FROM alert'
@@ -67,22 +61,20 @@ def create_app(test_config=None):
             ).fetchall()
 
             for alert in alerts:
-                print(alert['id'])
                 msg = Message(alert['title'], sender='backStopApp@gmail.com',
                             recipients=[alert['email']])
-                
                 msg.body = alert['message']
                 mail.send(msg)
+                database.execute(
+                    f"UPDATE alert set sent = true WHERE id = {alert['id']}"
+                )
                 return "Sent"
-
 
     scheduler = BackgroundScheduler()
     scheduler.add_job(send_overdue_emails, 'cron', minute='*/1')
     scheduler.start()
 
     return application
-
-
 
 
 
---
+++ flaskr/send_alert_email.py
@@ -0,0 +1,45 @@
+#TODO move mailer to here, currently having issue 
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask
+from flask_mail import Mail, Message
+from datetime import datetime
+# from flaskr.db import get_db
+from flask import current_app
+import logging
+
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+def create_alert_mailer(app):
+
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'backStopApp@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'afh8afh8'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    mail = Mail(app)
+
+
+    def send_overdue_emails():
+        with app.app_context():
+            now = datetime.now()
+            alerts = get_db().execute(
+                'SELECT a.id, title, schedule, email, message'
+                ' FROM alert'
+                f" WHERE a.schedule < {now}"
+            ).fetchall()
+
+            for alert in alerts:
+                msg = Message(alert['title'], sender='backStopApp@gmail.com',
+                    recipients=[alert['email']])
+                msg.body = alerts['message']
+                mail.send(msg)
+            return "Sent"
+
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(send_overdue_emails, 'cron', minute='*/1')
+    scheduler.start()
+
