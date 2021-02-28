86569f14736d9f0bbaf5cc741aa58f3fe16721ec
Patrick Duvall <duvallp208@gmail.com>
2021-02-28 10:33:36 -0700

    Add spec to see if background mailer adds job

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,33 +1,17 @@
-# import mock
-from pytest_mock import mocker
+
 import pytest
 from flaskr.db import get_db
 from flaskr import create_app
-import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 
-def test_send_overdue_emails(app):
-    with app.app_context():
-        db=get_db()
-        # uid = db.execute('SELECT id FROM user LIMIT 1;')
-        # db.execute(
-        #     'INSERT INTO alert (email, message, schedule, title, author_id)'
-        #     ' VALUES (?, ?, ?, ?, ?)',
-        #     ('test', 'message', '2020-01-01 00:00:02', 'title', g.user['id'])
-        # )
-        # db.commit()
-        create_app().send_overdue_emails()
-        alerts = db.execute('SELECT sent from ALERT')
-        for alert in alerts: 
-            assert alert == true
+# def test_send_overdue_emails(app):
+# TODO Currently this function is an inner funciton defined in create_app due
+# to the application factory pattern. The larger problem here is emails are being defined in
+# create app. This problem likely has to be solved before adding any additional mail schedulers
 
 
-def test_set_alert_scheduler(app):
-    with app.app_context():
-        # with patch.object(BackgroundScheduler(), 'add_job') as mock:
-        #     create_app()
-        # mock.assert_called_with(send_overdue_emails, 'cron', second='30')
-        spy = mocker.spy(create_app(), 'scheduler.addjob')
-        create_app()
-        spy.assert_called_once_with(21)
-
+def test_set_alert_scheduler(mocker):
+    spy = mocker.spy(BackgroundScheduler, 'add_job')
+    assert not spy.called
+    create_app()
+    assert spy.called
